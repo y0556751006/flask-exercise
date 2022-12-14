3aaa19dec92d7799c73340caf654ed0b978ff992
y0556751006 <y0556751006@gmail.com>
2022-12-14 13:38:07 +0200

    finished task

--- test_app.py
+++ test_app.py
@@ -1,76 +1,66 @@
-import json
-import unittest
 
-import requests
-url = "http://localhost:5000"
+# pytest automatically injects fixtures
+# that are defined in conftest.py
+# in this case, client is injected
 
-class MyTestCase(unittest.TestCase):
-    def test_index(self):
-        res = requests.get(url+"/")
-        assert res.status_code == 200
-        assert res.json()["result"]["content"] == "hello world!"
-
-    def test_mirror(self):
-        res = requests.get(url+"/mirror/Tim")
-        assert res.status_code == 200
-        assert res.json()["result"]["name"] == "Tim"
-
-    def test_get(self):
-        res = requests.get(url)
-        self.assertEqual(res.status_code, 200)
-        # assert res.status_code==200
-
-    # def test_get_users(self):
-    #     res = requests.get(url+"/users")
-    #     assert res.status_code == 200
-    #
-    #     res_users = res.json["result"]["user"]
-    #     assert len(res_users) == 4
-    #     assert res_users[0]["name"] == "Aria"
+def test_index(client):
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.json["result"]["content"] == "hello world!"
 
 
-    def tests_get_users_with_team(self):
-        res = requests.get(url+"/users?team=C2TC")
-        assert res.status_code == 200
-
-        res_users = res.json()["result"]["user"]
-        assert len(res_users) == 1
+def test_mirror(client):
+    res = client.get("/mirror/Tim")
+    assert res.status_code == 200
+    assert res.json["result"]["name"] == "Tim"
 
 
-    def test_get_user_id(self):
-        res = requests.get(url+"/users/1")
-        assert res.status_code == 200
+def test_get_users(client):
+    res = client.get("/users")
+    assert res.status_code == 200
 
-        res_user = res.json()["result"]["user"]
-        assert res_user["name"] == "Aria"
-        assert res_user["age"] == 19
-
-
-    def test_new_user(self):
-        user = {
-            "name": "Ron",
-            "age": 21,
-            "team": "NNB"
-               }
-        res = requests.post(url+"/users", json=user)
-        assert res.status_code == 201
-
-        res_user = res.json()["result"]["user"]
+    res_users = res.json["result"]["users"]
+    assert len(res_users) == 4
+    assert res_users[0]["name"] == "Aria"
 
 
-    def test_update(self):
-        data = {"team": "C2TC"}
-        res = requests.put(url+"/users/2", json=data)
-        assert res.status_code == 200
+def tests_get_users_with_team(client):
+    res = client.get("/users?team=LWB")
+    assert res.status_code == 200
 
-        res_user = res.json()["result"]["user"]
-        assert res_user["team"] == "C2TC"
-
-
-    def test_delete(self):
-        res = requests.delete(url+"/users/4")
-        assert res.status_code == 200
+    res_users = res.json["result"]["users"]
+    assert len(res_users) == 2
+    assert res_users[1]["name"] == "Tim"
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_user_id(client):
+    res = client.get("/users/1")
+    assert res.status_code == 200
+
+    res_user = res.json["result"]["user"]
+    assert res_user["name"] == "Aria"
+    assert res_user["age"] == 19
+
+
+def test_new_user(client):
+    res = client.post("/users", data={
+        "name": "Ron",
+        "age": 21,
+        "team": "NNB"
+    })
+    assert res.status_code == 200
+
+
+def test_update(client):
+    res = client.put("users/1", data={
+        "team": "C2TC"
+    })
+    assert res.status_code == 200
+
+    res_user = res.json["result"]["user"]
+    assert res_user[1]["team"] == "C2TC"
+
+
+def test_delete(client):
+    res = client.delete("users/3")
+    assert res.status_code == 200
